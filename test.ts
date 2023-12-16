ebaf41f8e4c22cf9861b96f6a13feb9bcf9d6d80
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-12-16 08:27:22 +0000

    more tests

--- test.ts
+++ test.ts
@@ -1,11 +1,89 @@
-// 
-faces.showFace(faces.Eyes.Open, faces.Mouth.Flat, 0, true)
 
-faces.showEyes(faces.Eyes.Open, 0, true)
+faces.showEyes(faces.Eyes.Open, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Sad, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Shut, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Mad, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Up, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Pop, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Left, 0, true);
+basic.pause(500);
+faces.showEyes(faces.Eyes.Right, 0, true);
+basic.pause(500);
 
-faces.showMouth(faces.Mouth.Flat, 0, true)
+faces.showMouth(faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Ok, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Grin, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Sulk, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Hmmm, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Open, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Left, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Right, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Shout, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Laugh, 0, true);
+basic.pause(500);
+faces.showMouth(faces.Mouth.Smirk, 0, true);
+basic.pause(500);
 
-faces.look(faces.EyesV.Up, faces.EyesH.Left, 0, true)
+faces.showFace(faces.Eyes.Open, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Sad, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Shut, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Mad, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Up, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Pop, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Left, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Right, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+
+faces.showFace(faces.Eyes.Open, faces.Mouth.Flat, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Ok, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Grin, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Sulk, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Hmmm, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Open, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Left, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Right, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Shout, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Laugh, 0, true);
+basic.pause(500);
+faces.showFace(faces.Eyes.Open, faces.Mouth.Smirk, 0, true);
+basic.pause(500);
+
+for (let v=0; v < 3; v++) {
+    for (let h = 0; h < 5; h++) {
+        faces.look(v, h, 0, true);
+    }
+}
 
 faces.wink(false, 750, true)
 
